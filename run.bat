3cb1e8ef3d626ef4e67ef68bbd598ca707f9ce17
Yashodha L <136436391+Dhanya9706@users.noreply.github.com>
2023-06-14 14:16:22 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
-python -m pytest -vsm regression --html=./reports/reg.html D:\Project\NopCommerce_FrameWork\testCases --browser chrome
-python -m pytest -vsm sanity --html=./reports/reg.html D:\Project\NopCommerce_FrameWork\testCases --browser chrome
-rem python -m pytest -vsm regression --html=./reports/reg.html D:\Project\NopCommerce_FrameWork\testCases --browser edge
-rem python -m pytest -vsm sanity --html=./reports/reg.html D:\Project\NopCommerce_FrameWork\testCases --browser edge
+rem python -m pytest -vsm regression --html=./reports/reg.html D:\Project\NopCommerce_FrameWork\testCases --browser chrome
+rem python -m pytest -vsm sanity --html=./reports/reg.html D:\Project\NopCommerce_FrameWork\testCases --browser chrome
+python -m pytest -vsm regression --html=./reports/reg.html D:\Project\NopCommerce_FrameWork\testCases --browser edge
+python -m pytest -vsm sanity --html=./reports/reg.html D:\Project\NopCommerce_FrameWork\testCases --browser edge
